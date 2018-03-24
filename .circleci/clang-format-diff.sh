f523c5628e95463456a17d9f1537afbb6b944690
doe300 <stadeldani@web.de>
2018-03-24 10:09:21 +0100

    Adds clang-format, see https://github.com/doe300/VC4C/pull/59

---
+++ .circleci/clang-format-diff.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+RET=0
+DIR=$(readlink -f $(dirname $0)/..)
+
+for file in src/*.cpp src/*/*.cpp include/*.h src/*.h src/*/*.h; do
+    clang-format $file > /tmp/file
+    A=$(diff $file /tmp/file)
+    if [ "$?" == 1 ] || [ "$RET" == 1 ]; then
+        RET=1
+        echo "file: $file"
+        echo "$A"
+    else
+        RET=0
+    fi
+done
+
+exit "${RET}"
